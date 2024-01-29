16e1b5d5b9fdd793b7eb3db705c91a0b4959c1ac
AyoubBenziza <ayoub.benziza@yahoo.com>
2024-01-29 03:28:16 +0100

    fix: Case Empty Comments

--- components/comment/CommentSection.tsx
+++ components/comment/CommentSection.tsx
@@ -17,32 +17,34 @@ export const CommentSection = async () => {
   return (
     <SectionWrapper id="comments" title="Comments">
       <div className="flex flex-col items-center w-full max-w-2xl gap-4">
-        <div className="grid justify-items-center w-full gap-4 my-4">
-          <Carousel className="w-3/4 content-center max-w-60 text-primary">
-            <CarouselContent>
-              {comments.map(
-                (comment: {
-                  id: string;
-                  username: string;
-                  comment: string;
-                  createdAt: Date;
-                }) => (
-                  <CarouselItem key={comment.id}>
-                    <div className="p-1">
-                      <Card className="bg-accent text-white">
-                        <CardContent className="flex items-center justify-center p-3">
-                          <Comment key={comment.id} {...comment} />
-                        </CardContent>
-                      </Card>
-                    </div>
-                  </CarouselItem>
-                )
-              )}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </div>
+        {comments.length ? (
+          <div className="grid justify-items-center w-full gap-4 my-4">
+            <Carousel className="w-3/4 content-center max-w-60 text-primary">
+              <CarouselContent>
+                {comments.map(
+                  (comment: {
+                    id: string;
+                    username: string;
+                    comment: string;
+                    createdAt: Date;
+                  }) => (
+                    <CarouselItem key={comment.id}>
+                      <div className="p-1">
+                        <Card className="bg-accent text-white">
+                          <CardContent className="flex items-center justify-center p-3">
+                            <Comment key={comment.id} {...comment} />
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                  )
+                )}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
+        ) : null}
         <CommentForm />
       </div>
     </SectionWrapper>
